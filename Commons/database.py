04a917fcecfaace47d0b387ab8f0ee0f55f71968
Kabir Sharma <kabirsharma2905@gmail.com>
2023-10-12 22:43:28 +0530

    replaced dockerfile with deploy.zsh

--- Commons/database.py
+++ Commons/database.py
@@ -1,27 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-from Commons.BuildDB import builddb
+from os import getenv
 
 # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-engine = create_engine('mysql+pymysql://root:1Madar2chod#@localhost:8000/iammusic')
+DB_STR = getenv('DB_STR')
+engine = create_engine(f'mysql+pymysql://{DB_STR}')
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = session.query_property()
 
-from .models import * #models
-Base.metadata.create_all(bind=engine)
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata. Otherwise
-    # you will have to import them first before calling init_db()
-    from . import models
-    Base.metadata.create_all(bind=engine)
-    builddb() #Use this method to insert Foundation data like Super User
-
-
-init_db()
+# Use database.sql file to create tables instead of create_all()
 
--- srvCore/__init__.py
+++ srvCore/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from os import getenv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Hello Mufkr"
 app.config['MEDIA_PATH'] = "Media/"
+app.config['SECRET_KEY'] = getenv('IAMM_API_KEY')
+app.config['LOGLVL'] = int(getenv('IAMM_LOGLEVEL'))
+app.config['LOG_PATH'] = getenv('IAMM_LOGFILE_PATH')
 
 from srvCore import routes
